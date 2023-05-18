858bff838512b6b843a6f5298a9a7a8cc69db475
mythily-muthu <mythilimuthu18@gmail.com>
2023-05-18 13:44:30 +0530

    Created pages using Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-
+import { BrowserRouter as Router } from "react-router-dom";
+import Topbar from "./components/Topbar";
+import Navbar from "./components/Navbar";
+import Add from "./components/Add";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="w-screen h-screen ">
-
+      <Router>
+        {/* headers */}
+        <Topbar />
+        <Navbar />
+        <Add />
+        {/* content */}
+        <Content />
+        {/* footer */}
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+const Content = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Content;
